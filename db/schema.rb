fcdbc3a1a80d6cc84964e84c5fbaf7d667c06fcf
inklin <twstacey@gmail.com>
2015-11-15 04:13:46 +0000

    Add url to form and messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115034825) do
+ActiveRecord::Schema.define(version: 20151115035403) do
 
   create_table "messages", force: :cascade do |t|
     t.string "author"
     t.string "content"
+    t.string "url"
   end
 
 end
